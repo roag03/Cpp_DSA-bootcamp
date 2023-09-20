1abe02b642a9e8c067996dec6751e30853e14204
roag03 <agraharidurgesh09@gmail.com>
2023-09-20 19:14:25 +0530

    day2

--- .vscode/function.cpp
+++ .vscode/function.cpp
@@ -3,19 +3,51 @@ using namespace std;
 
 // i am using function from this area/
 
-void pintName(){
-  int n;
-  cout<<"the value of n is"<<endl;
-  cin>>n;
-  for(int i=0; i<n; i++){
-    cout<<"Rohit will become worlds richest person with his friends"<<endl;
-  }
+// void pintName(){
+//   int n;
+//   cout<<"the value of n is"<<endl;
+//   cin>>n;
+//   for(int i=0; i<n; i++){
+//     cout<<"Rohit will become worlds richest person with his friends"<<endl;
+//   }
+// }
+// int add(int a,int b){
+//  int result=a+b;
+//  return result;
+// }
+// int main(){
+// // pintName();
+//     // int a= 5;
+//     // cin >>a;
+//     // cout<<"value of a "<<a<<endl;
+
+//     int a;
+//     cout<<"the value of a"<<endl;
+//     cin>>a;
+//     int b;
+//     cout<<"the value of b"<<endl;
+//     cin>>b;
+//     int sum=add(a,b);
+//     cout<<"the value of a+b is"<<sum<<endl;
+
+
+// cout<<"the address of a is " << &a <<endl;
+int value(int num1,int num2,int num3){
+    if (num1>num2 && num1>num3){
+        return num1;
+    }
+    else if(num2>num1 &&num2> num3){
+        return num2;
+    }
+    else{
+    return num3;
+}
 }
 int main(){
-pintName();
-    // int a;
-    // cin >>a;
-    // cout<<"value of a "<<a<<endl;
+int a,b,c;
+cin>>a>>b>>c;
+int maxNumber= value(a,b,c);
+cout<< maxNumber<<endl;
 
-    return 0;
+    
 }
---
+++ .vscode/function2.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+void printnumber(int n){
+    for(int i=0;i<n;i++){
+        cout<<i<< " ";
+    }
+    cout<<endl;
+}
+int getsum(int n){
+   int sum=0;
+   for(int i=2;i<=n;i=i+2){
+    sum=sum+i;
+   }
+return sum;
+}
+int main(){
+//     int a;
+//     cout<<"value odf n is"<<endl;
+//     cin>>a;
+//    printnumber(a);
+
+int a;
+cout<<"the valuue of a is";
+cin>>a;
+int psum=getsum(a);
+cout<<psum<<endl;
+}
